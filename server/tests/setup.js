3b89402335630d34ae5d0e6fdc21a65ede21ca62
Sachin-tec <sachinkethavath0@gmail.com>
2026-07-16 21:59:39 +0530

    done

--- server/tests/setup.js
+++ server/tests/setup.js
@@ -1,35 +1,37 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 const connectDB = require("../config/db");
-jest.setTimeout(30000);
-dotenv.config();
+
+jest.setTimeout(60000);
+
+let mongoServer;
 
 beforeAll(async () => {
-
     process.env.NODE_ENV = "test";
-
+    
+    // Create an in-memory MongoDB instance for testing
+    mongoServer = await MongoMemoryServer.create();
+    process.env.MONGODB_URI = mongoServer.getUri();
+    
     if (mongoose.connection.readyState === 0) {
         await connectDB();
     }
-
 });
 
 afterEach(async () => {
-
     const collections = mongoose.connection.collections;
-
     for (const key of Object.keys(collections)) {
         await collections[key].deleteMany({});
     }
-
 });
 
 afterAll(async () => {
     const collections = mongoose.connection.collections;
-
     for (const key of Object.keys(collections)) {
         await collections[key].deleteMany({});
     }
-
     await mongoose.connection.close();
+    if (mongoServer) {
+        await mongoServer.stop();
+    }
 });
